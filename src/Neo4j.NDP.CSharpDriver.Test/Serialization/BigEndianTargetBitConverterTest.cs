7242231e9dbdff3c7bf18b73cdc164c3d67618b7
Martin Jensen <ingvar@netaben.dk>
2015-06-12 23:20:18 +0200

    Removing unused using to clean the code.

--- src/Neo4j.NDP.CSharpDriver.Test/Serialization/BigEndianTargetBitConverterTest.cs
+++ src/Neo4j.NDP.CSharpDriver.Test/Serialization/BigEndianTargetBitConverterTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo4j.NDP.CSharpDriver.Serialization;
 
 namespace Neo4j.NDP.CSharpDriver.Test.Serialization
