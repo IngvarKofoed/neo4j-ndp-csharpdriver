75a4490f38058baa8b9f8cb68e92c5894dc0a6d6
Martin Jensen <ingvar@netaben.dk>
2015-06-13 19:36:54 +0200

    Adding a Connection class that thats all communication with the server on a heigh level.

---
+++ src/Neo4j.NDP.CSharpDriver/Connection.cs
@@ -0,0 +1,103 @@
+﻿using Neo4j.NDP.CSharpDriver.Logging;
+using Neo4j.NDP.CSharpDriver.Serialization;
+using System;
+using System.Net.Sockets;
+
+
+namespace Neo4j.NDP.CSharpDriver
+{
+    internal class Connection : IConnection
+    {
+        private const string initText = "ExampleDriver/1.0";
+
+        private readonly TcpClient client;
+        private readonly ChunkStream chunkStream;
+        private readonly ILogger logger;
+
+        public Connection(TcpClient client, NetworkStream stream, ILogger logger)
+        {
+            if (client == null) throw new ArgumentNullException("client");
+            if (stream == null) throw new ArgumentNullException("stream");
+
+            this.client = client;
+            this.chunkStream = new ChunkStream(stream);
+            this.logger = logger;
+
+            logger.Info("NDPv1 connection established!");
+
+            bool initialized = Initialize();
+            if (!initialized)
+            {
+                throw new InvalidOperationException("Failed to initialize the connection");
+            }
+
+            logger.Info("Initialization was successful");
+        }
+
+        public void Run(string statement)
+        {
+            logger.Info("Running statement: {0}", statement);
+            IMessageStructure runRequest = new MessageStructure(
+                StructureSignature.Run, new IMessageObject[] {
+					new MessageText(statement),
+					new MessageMap() // TODO: Put parameters here
+		    });
+            chunkStream.Write(runRequest);
+
+            IMessageStructure pullAllRequest = new MessageStructure(
+                StructureSignature.PullAll
+            );
+            chunkStream.Write(pullAllRequest);
+
+            IMessageObject runResponse = chunkStream.Read();
+            if (runResponse == null || runResponse.Type != MessageObjectType.Structure)
+            {
+                throw new InvalidOperationException("Unexpected data received: " + runResponse ?? "");
+            }
+
+            IMessageStructure runResponseStructure = runResponse as IMessageStructure;
+            if (runResponseStructure.Signature != StructureSignature.Success)
+            {
+                throw new InvalidOperationException("Run request failed with: " + runResponseStructure.ToString());
+            }
+
+            chunkStream.ReadEndOfMessage();
+
+            logger.Info("Statement ran with success");
+
+            while (true)
+            {
+                IMessageObject result = chunkStream.Read();
+                if (result == null)
+                    break;
+            }
+        }
+
+        public void Dispose()
+        {
+            logger.Info("Shutting down and closing connection");
+            chunkStream.Dispose();
+            ((IDisposable)client).Dispose();
+        }
+
+
+        private bool Initialize()
+        {
+            logger.Info("Initializing connection");
+
+            IMessageStructure initRequest = new MessageStructure(
+                StructureSignature.Init,
+                new MessageText(initText)
+            );
+
+            chunkStream.Write(initRequest);
+            IMessageStructure initResponse = chunkStream.Read() as IMessageStructure;
+            if (initResponse == null) return false;
+            if (initResponse.Signature != StructureSignature.Success) return false;
+
+            chunkStream.ReadEndOfMessage();
+
+            return true;
+        }
+    }
+}
---
+++ src/Neo4j.NDP.CSharpDriver/IConnection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Neo4j.NDP.CSharpDriver
+{
+    // TODO: This interface is not finished yet!
+    public interface IConnection : IDisposable
+    {
+        void Run(string statement);
+    }
+}
