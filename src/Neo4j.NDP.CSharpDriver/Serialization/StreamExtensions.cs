7e9efcc4d35ccc90e7481782dca10078832b6b31
Martin Jensen <ingvar@netaben.dk>
2015-06-13 18:10:18 +0200

    MessageObjectDeserializer added for deserializing responses from the server. Note: This is a poor implementation that is hard to test. This will be fixed later on.

---
+++ src/Neo4j.NDP.CSharpDriver/Serialization/StreamExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Neo4j.NDP.CSharpDriver.Serialization
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Writes a sequence of bytes with the length and given by <paramref name="data"/>
+        /// byte array.
+        /// </summary>
+        /// <param name="stream">The stream to write to.</param>
+        /// <param name="data">The byte array to write.</param>
+        public static void Write(this Stream stream, byte[] data)
+        {
+            if (data == null) throw new ArgumentNullException("data");
+            stream.Write(data, 0, data.Length);
+        }
+
+        /// <summary>
+        /// Reads a sequence of bytes with the length of <paramref name="data"/>
+        /// byte array.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <param name="data">The byte array to read into.</param>
+        public static void Read(this Stream stream, byte[] data)
+        {
+            if (data == null) throw new ArgumentNullException("data");
+            stream.Read(data, 0, data.Length);
+        }
+    }
+}
