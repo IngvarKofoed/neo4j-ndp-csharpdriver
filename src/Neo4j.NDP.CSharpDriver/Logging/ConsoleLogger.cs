e1745aee411473a621622c2f58bdf7a018271c5f
Martin Jensen <ingvar@netaben.dk>
2015-06-14 08:39:00 +0200

    Making the console logger more robust regarding restoring the forground color in case of exceptions

--- src/Neo4j.NDP.CSharpDriver/Logging/ConsoleLogger.cs
+++ src/Neo4j.NDP.CSharpDriver/Logging/ConsoleLogger.cs
@@ -11,36 +11,41 @@ namespace Neo4j.NDP.CSharpDriver.Logging
         {
             ConsoleColor oldColor = Console.ForegroundColor;
 
-            string prefix = "";
-            if (severity == LogSeverity.Debug)
+            try
             {
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                prefix = "DEBUG";
-            }
-            else if (severity == LogSeverity.Information)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                prefix = "INFO ";
-            }
-            else if (severity == LogSeverity.Warning)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                prefix = "WARN ";
-            }
-            else if (severity == LogSeverity.Error)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                prefix = "ERROR";
-            }
-            else if (severity == LogSeverity.Fatal)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                prefix = "FATAL";
-            }
+                string prefix = "";
+                if (severity == LogSeverity.Debug)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    prefix = "DEBUG";
+                }
+                else if (severity == LogSeverity.Information)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    prefix = "INFO ";
+                }
+                else if (severity == LogSeverity.Warning)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    prefix = "WARN ";
+                }
+                else if (severity == LogSeverity.Error)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    prefix = "ERROR";
+                }
+                else if (severity == LogSeverity.Fatal)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    prefix = "FATAL";
+                }
 
-            Console.WriteLine(prefix + ": " + string.Format(format, arguments));
-
-            Console.ForegroundColor = oldColor;
+                Console.WriteLine(prefix + ": " + string.Format(format, arguments));
+            }
+            finally
+            {
+                Console.ForegroundColor = oldColor;
+            }
         }
     }
 }
